0bacc8eed9932f23e62b79957c3f95b96a65ba39
Jose Miguel Valiente Niño <valiente_jose2@hotmail.com>
2020-04-04 09:49:34 +0200

    Init and route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/auth/Login'
+import AddUser from './components/auth/AddUser'
+import Proyects from './components/proyect/Proyects'
+import {Route , Switch, Link} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path = '/' component={Login}/>
+        <Route exact path = '/create-user' component={AddUser}/>
+        <Route exact path = '/proyects' component={Proyects}/>
+      </Switch>
     </div>
   );
 }
